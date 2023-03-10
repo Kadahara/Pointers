160ce2c4c53e42ed176d6b7b58c41451895d75e5
User <User@WIN-E8F88FA2UKH>
2023-03-10 11:21:29 +0300

    Ссылки

---
+++ References/maim.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+#define tab "\t"
+void Exchange(int& a, int& b);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	//reference 
+	int a = 2, b = 3;
+	cout << a << tab << b << endl;
+	Exchange(a, b);
+	cout << a << tab << b << endl;
+}
+
+void Exchange(int& a, int& b)
+{
+	int buffer = a;
+	a = b;
+	b = buffer;
+}
